070fa132032ce1b8ff5d0eb1c2047648680738f0
ahmedalsaedi21 <153505113+ahmedalsaedi21@users.noreply.github.com>
2023-12-14 18:49:00 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,33 +43,20 @@ $(function(){
 })
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml1 .letters');
+var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml1 .letter',
-    scale: [0.3,1],
+    targets: '.ml3 .letter',
     opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 600,
-    delay: (el, i) => 70 * (i+1)
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
   }).add({
-    targets: '.ml1 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700,
-    offset: '-=875',
-    delay: (el, i, l) => 80 * (l - i)
-  }).add({
-    targets: '.ml1',
+    targets: '.ml3',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
   });
-
-  
-  
